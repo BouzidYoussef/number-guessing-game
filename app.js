9b74bf15545488e5373f20b49d6f5e57f3d74f25
BouzidYoussef <bouzidyoussef600@yahoo.com>
2021-07-27 23:06:00 +0100

    setting up speech recognition

--- app.js
+++ app.js
@@ -2,8 +2,25 @@ const replyEl = document.getElementById('reply')
 
 const randomNum = getRandomNumber()
 
+console.log('Number', randomNum)
 
+window.SpeechRecognition = 
+window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+//Starts recognition 
+recognition.start();
+
+//Capture user speak
+function onSpeak(e){
+    const reply = e.results[0] [0].transcript;
+    // writeMessage(reply);
+    // checkNumber(reply);
+}
+
+//Generate randomNumber
 function getRandomNumber(){
     return Math.floor(Math.random() * 100) + 1;
-    
 }
+//Speak result
+recognition.addEventListener('result', onSpeak())
