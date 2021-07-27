adcc8acdf7699e552e0c33d6539e7f63b17dc851
BouzidYoussef <bouzidyoussef600@yahoo.com>
2021-07-27 23:14:46 +0100

    number check

--- app.js
+++ app.js
@@ -18,6 +18,32 @@ function onSpeak(e){
     // checkNumber(reply);
 }
 
+//write user speech
+    function writeMessage(reply){
+        replyEl.innerHTML=
+        <div>You said:</div>
+        <span class='box'>${reply}</span>
+        ;
+    }
+
+    //check if reply is a number
+    function checkNumber(reply){
+        const num =+reply
+        if(Number.isNaN(num)){
+        replyEl.innerHTML = <div>It's not a number</div> 
+            return;
+    }
+
+    //check if the number is in range of 1 - 100
+    if(num > 100 || num < 1){
+        replyEl.innerHTML= 
+        <div>Number must be between 1 and 100</div>;
+    }    
+
+}
+
+
+
 //Generate randomNumber
 function getRandomNumber(){
     return Math.floor(Math.random() * 100) + 1;
